2d3729e9bca035a9671f6f92d97be483873b3516
Ahmed Essam <Ahmed.Essam28677@gmail.com>
2023-11-19 02:45:21 +0200

    V1

--- App/Main/main.c
+++ App/Main/main.c
@@ -6,11 +6,42 @@
  */ 
 
 
+#include "PORT_Core.h"
+#include "IRQH_Core.h"
+#include "ULTRA_SONIC.h"
+#include "PWM_Core.h"
+#include "ICU_Core.h"
+
+
+
+
+#define  F_CPU  16000000U
+#include <util/delay.h>
+
 int main(void)
-{	
+{
+	
+	PORT_Init();
+	IRQH_SetGloabINT(INT_ENABLE);
+	PWM_Init();
+	ICU_Init();
+	LCD_Init();
+	
+	PWM0_Generate(74);
 	while (1)
 	{
-
+		ICU_Pulse();
+		
+		LCD_GoTo(0,0);
+		LCD_WriteString("Duty Cycle:");
+		LCD_WriteInteger( Duty_CY);
+		LCD_WriteString("%");
+		
+		LCD_GoTo(1,0);
+		LCD_WriteString("Freq:");
+		LCD_WriteInteger(Freq);
+		LCD_WriteString("Hz");
+		_delay_ms(1000);
 	}
 }
 
